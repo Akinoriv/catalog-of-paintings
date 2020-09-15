4896a0caf43c28361f601fc78b0eb8f88cc3c8f5
akinoriv <nika.nika1711@gmail.com>
2020-09-15 18:39:34 +0300

    готовая шапка

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <img class="logo" src="./assets/Group.png" alt="logo" />
-     -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
+    <div class="header">
+      <img src="https://raw.githubusercontent.com/Akinoriv/Redsoft-test/5fe5899fab4c6a279e8dba464688de7aea775aa6/my-project/src/assets/Group.svg" class="logo"> 
+      <nav>
+        <ul class="nav">
+          <li>Каталог</li>
+          <li>Доставка</li>
+          <li>Оплата</li>
+          <li>Контакты</li>
+          <li>О галерее</li>
+          <input v-model="message" placeholder="Поиск по названию картины">
+          <button>Найти</button>
+        </ul>
+      </nav>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -20,12 +32,73 @@ export default {
 
 <style>
 
+img {
+  height: 48px;
+  width: 48px;
+  border: none;
+  align-self: center;
+}
+
+button {
+  border: none;
+  height: 48px;
+  width: 112px;
+  background-color: #382E2B;
+  color: #F4F6F9;
+  Font-Family: Merriweather;
+  Font-Style: Bold;
+  Font-Size: 14px;
+  Line-Height: 21px;
+  Line-Height: 150%;
+  align-self: Center;
+  
+  justify-content:right;
+}
+
+input {
+  border: none;
+  height: 48px;
+  width: 416px;
+  background-color: #E1E1E1;
+  color: #9F9F9F;
+  align-self : Inside;
+  Font-Family: Merriweather;
+  Font-Style: Regular;
+  Font-Size: 14px;
+  Line-Height: 21px;
+  Line-Height: 150%;
+  justify-content:right; 
+  margin-left: 150px; 
+
+}
+
+.header {
+  display: flex;
+  width: 100%;
+  max-width: 1338.5px;
+  margin: 0 auto;
+  
+}
+
+.nav {
+  display: flex;
+  list-style-type: none;
+}
+  .nav>li {
+        margin-left: 24px;
+        margin-right: 24px;
+        justify-content:start;  
+        align-self: Center; 
+      }
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Merriweather', serif;
+  color: #343030;
+  Font-Style: Regular;
+  Font-Size: 14px;
+  Line-Height: 21px;
+  Line-Height: 150%;
+  background:#E5E5E5;
+  Fill: Solid
 }
 </style>
